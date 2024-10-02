e1dfd1d032bc6a75eca4ef05f7c84a97f9a45894
Taisuke Fukuno <fukuno@jig.jp>
2024-10-03 07:20:08 +0900

    Initial commit

---
+++ scrapeutil.js
@@ -0,0 +1,21 @@
+export { fetchOrLoad } from "https://js.sabae.cc/fetchOrLoad.js";
+export { HTMLParser } from "https://js.sabae.cc/HTMLParser.js";
+export { CSV } from "https://code4fukui.github.io/CSV/CSV.js";
+export { sleep } from "https://js.sabae.cc/sleep.js";
+export { table2json } from "https://codeforkosen.github.io/kosenconf-opendata/table2json.js";
+export { table2csv } from "https://codeforkosen.github.io/kosenconf-opendata/table2csv.js";
+
+export const nextTag = (tag) => {
+  const nodes = tag.parentNode.childNodes;
+  for (let i = 0; i < nodes.length; i++) {
+    if (nodes[i] == tag) return nodes[i + 1];
+  }
+  return null;
+};
+export const prevTag = (tag) => {
+  const nodes = tag.parentNode.childNodes;
+  for (let i = 0; i < nodes.length; i++) {
+    if (nodes[i] == tag) return nodes[i - 1];
+  }
+  return null;
+};
